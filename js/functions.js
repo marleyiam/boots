c998717115f9e0818658a2dd567000a0664819e5
marley ferreira <marleysheredder@gmail.com>
2013-05-18 22:55:09 -0300

    implementando file upload

---
+++ js/functions.js
@@ -0,0 +1,42 @@
+
+Object.values = function (obj) {
+	var vals = [];
+	for( var key in obj ) {
+		if ( obj.hasOwnProperty(key) ) {
+			vals.push(obj[key]);
+		}
+	}
+	return vals;
+}
+
+function paramDictionary(obj){
+	key = obj;
+	str ='';
+	switch(key){
+		case 'name':
+		str = 'nome' ;
+		break;
+		case 'price':
+		str = 'preço' ;
+		break;
+		case 'category_ids':
+		str = 'categorias' ;
+		break;
+		case 'tag_list':
+		str = 'tags' ;
+		break;
+		case 'place':
+		str = 'local' ;
+		break;
+		default:
+		str = 'none' ;
+		break;
+	}
+	return str;
+}
+
+function printError(obj){
+	reposta = obj.responseText;
+	reposta = JSON.parse(reposta);
+	return paramDictionary(Object.keys(reposta)[0])+' '+Object.values(reposta)[0];
+}
